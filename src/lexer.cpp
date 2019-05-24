b0dc35c929eee00f5a5cf3977a087f14bfe3728b
Justin Haupt <jhaupt100@gmail.com>
2019-05-24 13:21:56 -0700

    write definition for lexer source

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -2,6 +2,19 @@
 #include "parser.h"
 
 
-Lexer::Lexer() {}
+Lexer::Lexer(): input(0), token(START) {}
 
-Token::Token() {}
+
+// Reads in a new source of raw characters.
+// First deletes the previous set of characters,
+// then copies in the new set from user input.
+void Lexer::source(char * usr_input) {
+  if(input)
+      delete [] input;
+
+  input = new char[strlen(usr_input) + 1];
+  strcpy(input, usr_input);
+
+  std::cout << '\n' << input << '\n';
+
+}
