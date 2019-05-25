2b44914ccba3a436356254375b2f162273d18f52
Justin Haupt <jhaupt100@gmail.com>
2019-05-24 19:23:49 -0700

    implement parser functions

--- src/parser.cpp
+++ src/parser.cpp
@@ -2,3 +2,74 @@
 #include "parser.h"
 
 Parser::Parser(Lexer l): lexer(l) {}
+
+
+
+Expression * Parser::expr() {
+  Expression * l = add();
+  if (lexer.get_token() == SUB) {
+      lexer.next_token();
+      return new Sub(l, expr());
+  }
+  return l;
+}
+
+Expression * Parser::add() {
+  Expression * l = div();
+  if (lexer.get_token() == ADD) {
+      lexer.next_token();
+      return new Add(l, expr());
+  }
+  return l;
+}
+
+Expression * Parser::div() {
+  Expression * l = mul();
+  if (lexer.get_token() == DIV) {
+      lexer.next_token();
+      return new Div(l, expr());
+  }
+  return l;
+}
+
+Expression * Parser::mul() {
+  Expression * l = exp();
+  if (lexer.get_token() == MUL) {
+      lexer.next_token();
+      return new Mul(l, expr());
+  }
+  return l;
+}
+
+Expression * Parser::exp() {
+  Expression * l = atom();
+  if (lexer.get_token() == EXP) {
+      lexer.next_token();
+      return new Exp(l, expr());
+  }
+  return l;
+}
+
+Expression * Parser::atom() {
+  switch (lexer.get_token()) {
+    case NUM: {
+      double value = lexer.get_lexeme();
+      lexer.next_token();
+      return new Num(value);
+    }
+    case OPEN: {
+      lexer.next_token();
+      Expression * e = expr();
+      if (lexer.get_token() == ')') {
+        lexer.next_token();
+      } else {
+        std::cout << "Error! Missing close paren";
+      }
+      return e;
+    }
+    default: {
+      std::cout << "Error!";
+      return NULL;
+    }
+  }
+}
--- src/parser.h
+++ src/parser.h
@@ -7,12 +7,12 @@ class Parser {
   public:
     Parser(Lexer l);
 
-    Expression expr();
-    Expression add();
-    Expression div();
-    Expression mul();
-    Expression exp();
-    Expression atom();
+    Expression* expr();
+    Expression* add();
+    Expression* div();
+    Expression* mul();
+    Expression* exp();
+    Expression* atom();
 
   private:
     Lexer lexer;
